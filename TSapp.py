95cefb37d24b005d2ca4745d0e840c47b282610c
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-03-04 18:01:49 +0100

    Update TSapp.py

--- TSapp.py
+++ TSapp.py
@@ -3,3 +3,8 @@ import warnings
 warnings.filterwarnings('ignore')
 import numpy as np 
 import pandas as pd
+
+
+google = pd.read_csv('../input/stock-time-series-20050101-to-20171231/GOOGL_2006-01-01_to_2018-01-01.csv', index_col='Date', parse_dates=['Date'])
+
+st.dataframe(google)
