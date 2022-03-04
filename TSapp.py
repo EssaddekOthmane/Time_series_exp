c6cf2539061ae33bd4ca51fcc16764a2a7c128b6
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-03-04 18:16:52 +0100

    Update TSapp.py

--- TSapp.py
+++ TSapp.py
@@ -5,6 +5,6 @@ import numpy as np
 import pandas as pd
 
 
-google = pd.read_csv('../input/stock-time-series-20050101-to-20171231/GOOGL_2006-01-01_to_2018-01-01.csv', index_col='Date', parse_dates=['Date'])
+wind_direction = pd.read_csv('wind_direction.csv')
 
-st.dataframe(google)
+st.dataframe(wind_direction)
