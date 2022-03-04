091ff519f00bfb6d04a8edca4f43a42d50da8706
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-03-04 18:21:02 +0100

    Update TSapp.py

--- TSapp.py
+++ TSapp.py
@@ -6,6 +6,6 @@ import pandas as pd
 import streamlit as st
 
 
-wind_direction = pd.read_csv('wind_direction.csv', index_col='datetime', parse_dates=['datetime']))
+wind_direction = pd.read_csv('wind_direction.csv', index_col='datetime', parse_dates=['datetime'])
 
 st.dataframe(wind_direction)
