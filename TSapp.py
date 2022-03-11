4269c978cf7cd4a88b79a1a08b13004f35e4cd4a
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-03-11 16:21:26 +0100

    Update TSapp.py

--- TSapp.py
+++ TSapp.py
@@ -31,8 +31,8 @@ option_freq=st.multiselect('veuillez selectionner une base pour la fréquence :'
 
 #num=st.slider( f"For the parameter: {k}",step= (l[1]-l[0]),min_value=l[0], max_value=l[-1],value= l[-1]) 
 if (option_freq!='none'):
-    fr=energy["PJM_Load_MW"].asfreq(f"{1}{option_freq}") 
-    fr=pd.DataFrame(energy["PJM_Load_MW"].asfreq(f"{2}{option_freq}"))
+    #fr=energy["PJM_Load_MW"].asfreq(f"{1}{option_freq}") 
+    fr=pd.DataFrame(energy["PJM_Load_MW"].asfreq(f"{1}{option_freq}"))
     frr=fr.copy()
     frr['ind']=[k for k in range(len(frr["PJM_Load_MW"]) )]
     frr.reset_index(inplace=True)
