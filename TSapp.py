98672cb33c0cfef474874ad420a2493b87dc2f55
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-03-11 20:48:05 +0100

    Update TSapp.py

--- TSapp.py
+++ TSapp.py
@@ -20,6 +20,32 @@ from numpy.random import *
 
 
 
+def orstein_uhlenbeck2(T,n,X_0,theta,u,sigma):
+    db=np.random.normal(0,sqrt(T/n),n)
+    dt=T/n
+    X=np.zeros(n)
+    X[0]=X_0
+    for i in range(1,n):
+        X[i]=X[i-1]+ theta*(u-X[i-1])*dt+sigma*db[i-1]
+    return(X)
+
+
+def fe(x,c):
+    f=c
+    for i in range(5):
+        f=f+a[i]*(1+np.cos(2*np.pi*lam[i]*x))+b[i]*(np.sin(0.3333*2*np.pi*lam[i]*x))
+    return(f)
+
+c=5
+a=0.1*np.array([10,1,.5,.1,.01,.001])
+b=0.1*np.array([10,1,.5,.1,.01,.001])
+lam=np.array([1.8,.005,.25,366/7,366/7,366/7])                   #[20,.5,.1,30*365/7,1*20*365/7,40*365/7])
+
+
+t_=np.linspace(0,1,366 )
+
+f_t=np.array([fe(t_[i],c) for i in range(366 )])
+
 st.title("Consomation d'energie")
 st.subheader("1.Visualisation")
 st.markdown("On commence par regarder la dinamique de notre série temporelle, on vous propose alors de choisir la fréquense à laquelle vous souhaitez moyenniser la serie. ")
@@ -61,6 +87,32 @@ st.latex(r'''
 st.latex(r'''
     f(t)=c+\sum_{i=1}^6a_icos(2\pi\lambda_it)
      ''')
+
+
+
+ou_=orstein_uhlenbeck2(1,366 ,0,100,0,8)
+#energy["PJM_Load_MW"].asfreq('1d').plot()
+s_t=25000*np.random.uniform(0.95,1,366)+5*np.exp(f_t+0.5*ou_)#f_t*(1+0.005*(np.exp(ou_)+.001*f_t*ou_))
+E_d=energy["PJM_Load_MW"]['1999-01-01':'2000-01-01'].asfreq('1d')#.shift(25)
+dff=pd.DataFrame(E_d)
+ind=E_d.index
+df_es=pd.DataFrame(s_t)
+#df_es.columns=['Datetime','O_U']
+df_es.columns=['estim']
+df_es.set_index(ind)
+dff["estim"]=s_t
+
+# test=dff.copy()
+# test['ind']=[k for k in range(366 )]
+# test.reset_index(inplace=True)
+# line1=alt.Chart(test).mark_line().encode(
+#     x='Datetime',
+#     y='PJM_Load_MW'
+# )
+# line2=alt.Chart(test).mark_line(color='red').encode(
+#     x='Datetime',
+#     y='estim_sh'
+# )
 
 #wind_direction = pd.read_csv('wind_direction.csv', index_col='datetime', parse_dates=['datetime'])
 
