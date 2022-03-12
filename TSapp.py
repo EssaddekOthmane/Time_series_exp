cb44f210da15ca4c968320448f270dd622dbe755
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-03-12 11:26:32 +0100

    Update TSapp.py

--- TSapp.py
+++ TSapp.py
@@ -39,14 +39,14 @@ def fe(x,c):
     return(f)
 
 c=5
-a=0.1*np.array([10,1,.5,.1,.01,.001])
-b=0.1*np.array([10,1,.5,.1,.01,.001])
-lam=np.array([1.8,.005,.25,366/7,366/7,366/7])                   #[20,.5,.1,30*365/7,1*20*365/7,40*365/7])
+a=0.1*np.array([10,.5,.5,.25,.1,.1])
+b=0.1*np.array([10,.5,.5,.025,.01,-.1])
+lam=np.array([2.1,.75,2.5,0.1*365/7,0.1*365/7,0.1*365/7])                   #[20,.5,.1,30*365/7,1*20*365/7,40*365/7])
 
 
-t_=np.linspace(0,1,366 )
+t_=np.linspace(0,1,733 )
 
-f_t=np.array([fe(t_[i],c) for i in range(366 )])
+f_t=np.array([fe(t_[i],c) for i in range(733 )])
 
 st.title("Consomation d'energie")
 st.subheader("1.Visualisation")
@@ -88,9 +88,10 @@ st.latex(r'''
      ''')
 
 
-ou_=orstein_uhlenbeck2(1,366 ,0,100,0,8)
+#f_t=np.array([fe(t_[i],c) for i in range(733 )])
+ou_=orstein_uhlenbeck2(1,733 ,0,100,0,10)
 #energy["PJM_Load_MW"].asfreq('1d').plot()
-s_t=25000*np.random.uniform(0.95,1,366)+5*np.exp(f_t+0.5*ou_)#f_t*(1+0.005*(np.exp(ou_)+.001*f_t*ou_))
+s_t=23000*np.random.uniform(0.95,1,733)+10*np.exp(f_t+0.5*ou_)#f_t*(1+0.005*(np.exp(ou_)+.001*f_t*ou_))
 E_d=energy["PJM_Load_MW"]['1999-01-01':'2000-01-01'].asfreq('1d')#.shift(25)
 dff=pd.DataFrame(E_d)
 ind=E_d.index
@@ -101,7 +102,7 @@ df_es.set_index(ind)
 dff["estim"]=s_t
 
 test=dff.copy()
-test['ind']=[k for k in range(366 )]
+test['ind']=[k for k in range(733 )]
 test.reset_index(inplace=True)
 line1=alt.Chart(test).mark_line().encode(
     x='Datetime',
